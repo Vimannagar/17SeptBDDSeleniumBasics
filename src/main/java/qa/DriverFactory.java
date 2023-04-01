8ffaba2b5ff3c1a56be4cf443b9fa260b1ca8aa7
= <=>
2023-04-01 09:11:42 +0530

    selenium integration

---
+++ src/main/java/qa/DriverFactory.java
@@ -0,0 +1,14 @@
+package qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+	
+	
+	public void initBrowser()
+	{
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
